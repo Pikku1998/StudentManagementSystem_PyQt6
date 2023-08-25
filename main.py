68519f9cec649f788dac3f4b1ab054e43c4ce66f
Prakash R <prakashramanukolanu1998@gmail.com>
2023-08-25 12:40:55 +0530

    Add a table to the main window

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -6,6 +6,7 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.resize(410, 400)
         self.setWindowTitle("Student Management System")
 
         # Define Menu items
@@ -17,6 +18,11 @@ class MainWindow(QMainWindow):
         file_menu_item.addAction(add_student_action)
         about_action = QAction('About', self)
         help_menu_item.addAction(about_action)
+
+        self.student_table = QTableWidget()
+        self.student_table.setColumnCount(4)
+        self.student_table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile no.'))
+        self.setCentralWidget(self.student_table)
 
 
 app = QApplication(sys.argv)
